37f79f79b85e9c3e2d8fc65abdeeb933f1259633
uJZk <34388941+uJZk@users.noreply.github.com>
2024-11-16 19:38:57 -0600

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,3 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages-backup' >>feeds.conf.default
