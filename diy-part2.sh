d6cd9913109dd0e92fc56224b4613d5ed2d8fa5e
uJZk <34388941+uJZk@users.noreply.github.com>
2024-11-19 10:53:37 -0600

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,6 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 
 # Replace ash with bash
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+
+# Partition alignment
+sed -i 's/256/4096/g' target/linux/x86/image/Makefile
