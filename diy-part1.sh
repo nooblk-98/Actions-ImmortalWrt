33cf3409ab61f701b144ae8095d739fc774c8b39
uJZk <34388941+uJZk@users.noreply.github.com>
2024-12-25 08:51:32 +0800

    modified:   .github/workflows/build-immortalwrt.yml 	modified:   diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,13 +10,13 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
+# Uncomment feed sources
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Remove a feed source
+# Remove feed sources
 sed -i '/telephony/d' feeds.conf.default
 
-# Add a feed source
+# Add feed sources
 echo 'src-git daed https://github.com/QiuSimons/luci-app-daed' >> feeds.conf.default
 echo 'src-git wrtbwmon https://github.com/brvphoenix/luci-app-wrtbwmon' >> feeds.conf.default
 echo 'src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
