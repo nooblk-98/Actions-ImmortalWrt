dcf1a57230c0a6bf73593378897185d7ec9a238d
uJZk <34388941+uJZk@users.noreply.github.com>
2025-04-14 11:00:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,6 @@ sed -i '/telephony/d' feeds.conf.default
 
 # Add feed sources
 echo 'src-git daed https://github.com/QiuSimons/luci-app-daed' >> feeds.conf.default
-echo 'src-git wrtbwmon https://github.com/brvphoenix/luci-app-wrtbwmon' >> feeds.conf.default
+echo 'src-git wrtbwmon https://github.com/brvphoenix/wrtbwmon' >> feeds.conf.default
+echo 'src-git wrtbwmon_luci https://github.com/brvphoenix/luci-app-wrtbwmon' >> feeds.conf.default
 echo 'src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
